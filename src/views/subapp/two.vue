4951daf67cb95249caeb5ee3bfddff671f125317
zql <2576029557@qq.com>
2025-08-29 10:13:44 +0800

    fix

--- src/views/subapp/two.vue
+++ src/views/subapp/two.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="sub-app">我是vue2子应用页面2222222222</div>
+  <div class="sub-app">项目二地址two</div>
 </template>
 
 <style lang="scss" scoped>
 .sub-app {
   cursor: pointer;
-  background-color: aqua;
+  font-size: 24px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-color: cadetblue;
+  height: 300px;
 }
 </style>
+<script setup lang="ts">
+</script>
